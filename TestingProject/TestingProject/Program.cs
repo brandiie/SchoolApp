ebff7157dd34fadfbdcb8edfb9d4ac3ae52db4b6
gacumadeguzman <alejandrodeguzman1@gmail.com>
2017-04-24 11:04:51 +0800

    connect

---
+++ TestingProject/TestingProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1 + 1;
+            Console.WriteLine("Hello GIT! {0}", a);
+            Console.Read();
+        }
+    }
+}
